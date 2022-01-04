3ee80b931165d81f220f0aa57d192507a105d666
aniketbhaleraogithub <44803603+aniketbhaleraogithub@users.noreply.github.com>
2022-01-04 13:02:14 +0530

    Update Contains Duplicate .java

--- Contains Duplicate .java
+++ Contains Duplicate .java
@@ -14,4 +14,16 @@ class Solution {
 }
 
 /*
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        
+        for(int i = 0; i<nums.length; i++) {
+            if(!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
 */
